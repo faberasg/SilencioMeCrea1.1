2738957ccfe88f0b345810feae89838364749755
faberasg <alexandergoez103@gmail.com>
2019-06-10 17:27:54 -0500

    Actualización de datos

--- Proyecto/inicio.php
+++ Proyecto/inicio.php
@@ -15,12 +15,16 @@ $resultado = mysqli_query($conexion,$sql);
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" 
+  integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 
   <title>El silencio me crea</title>
 
   <link rel="stylesheet" href="css/bootstrap.min.css">
   <link rel="icon" href="img/icono.png" type="icono/png" sizes="16x16">
   <link rel="stylesheet" href="css/estilos.css">
+
+
   <script src="js/jquery-3.3.1.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
   <script src="js/bootstrap.bundle.min.js"></script>
@@ -51,21 +55,20 @@ $resultado = mysqli_query($conexion,$sql);
 
 
     <header id="header" class="row">
-    	<img id="logo" class="img col-12 col-sm-7 col-md-7 col-lg-5 src="img/logo.png" >  
+    	<img id="logo" class="img col-12 col-sm-7 col-md-7 col-lg-5" src="img/logo.png" >  
     </header>
 
     <!-------------------------------navegación-------------------------------------------------------------------------------------->
+    
+    <nav id="menu" class="color-nav row navbar navbar-expand-lg navbar-dark sticky-top">
 
-    <nav id="menu" class="color-nav row navbar navbar-expand-lg sticky-top">
-
-      <button class=" btn-outline-primary navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> 
+      <button class="btn-outline-primary navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> 
         <span class="navbar-toggler-icon"></span>
       </button>
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <div class="navbar-nav mr-auto ml-auto text-center">
-
-          <li class="nav-item nav-link active">INICIO </li>
+          <li class="nav-item nav-link active"><i class="fas fa-home" style='font-size:20px'></i>  </li>
           <li class="nav-item nav-link">GALERÍA</li>
           <li class="nav-item nav-link">INFORMACIÓN</li>
           <li class="nav-item nav-link">CUENTO</li>
@@ -74,8 +77,23 @@ $resultado = mysqli_query($conexion,$sql);
 
       <form class="form-inline my-2 my-lg-0 d-none d-sm-block">
       
-      <button class="btn btn-outline-light my-2 my-sm-0" type="submit"><a class="btin" href="registro.html">Registrate</a></button>
-      <button class="btn btn-outline-light my-2 my-sm-0" type="submit"><a class="btin" href="ContactoA.php">Contacto</a></button>
+      <li class="nav-item dropdown">
+        
+        <i class="nav-link fas fa-align-right" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" 
+        aria-haspopup="true" aria-expanded="false" style='font-size:20px'></i>
+        
+        <div class=" dropdown-menu" aria-labelledby="navbarDropdown">
+
+        <button class="des btn btn-outline-light  my-2 my-sm-0" type="submit"><a class="btin dropdown-item" 
+        href="registro.html"> Registrate   <i class="fas fa-registered"></i> </a></button>
+
+        <button class="dest btn btn-outline-light   my-2 my-sm-0" type="submit"><a class="btin dropdown-item" 
+        href="ContactoA.php"> Contacto   <i class="fas fa-address-book"></i></a></button>
+
+         
+        </div>
+      </li>
+      
       
     </form>
 
@@ -109,11 +127,11 @@ $resultado = mysqli_query($conexion,$sql);
   echo "</div>";
   echo "<a class='carousel-control-prev' href='#carouselExampleFade' role='button' data-slide='prev'>";
     echo "<span class='arousel-control-prev-icon' aria-hidden='true'></span>";
-    echo "<span class='sr-only'>Previous</span>";
+    echo "<span class='sr-only'>Anterior</span>";
   echo "</a>";
   echo "<a class='carousel-control-next' href='#carouselExampleFade' role='button' data-slide='next'>";
     echo "<span class='carousel-control-next-icon' aria-hidden='true'></span>";
-    echo "<span class='sr-only'>Next</span>";
+    echo "<span class='sr-only'>Siguiente</span>";
   echo "</a>";
 echo "</div>";
 echo "</section>";
@@ -153,18 +171,88 @@ echo "<div class='col-12 col-sm-12 col-md-12 col-lg-12'>";
  
 
 
-<section id='seccion2' class="row video">
-<div class='col-12  col-sm-12 col-md-12 col-lg-12' id="video">
+<section id='seccion2' class="row">
+<!--<div class='col-12  col-sm-12 col-md-12 col-lg-12' id="video">
     
 
 <video  controls autoplay loop style="
     width: 90%;
-    height: 100%;
     position: relative;
     left: 6%;
+    top: 3vh;
+    
 "> <source src="video/video.mp4" type="video/mp4"></video>
   
 </div>
+
+</section>
+<section class="row justify-content-center">
+<nav aria-label="Page navigation example" style="position:relative; top:4vh;">
+<h5 id="titu" style="margin-left:25%;">instructivo</h5>
+  <ul class="pagination justify-content-center">
+  
+    <li class="page-item disabled">
+      <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Anterior</a>
+    </li>
+    <li class="page-item "><a class="page-link" video="video.mp4">1</a></li>
+    <li class="page-item"><a class="page-link"  video="proyecto.mp4">2</a></li>
+    <li class="page-item"><a class="page-link" video="instructivo.mp4">3</a></li>
+    <li class="page-item">
+      <a class="page-link" href="#">Siguiente</a>
+    </li>
+  </ul>
+</nav>-->
+
+<div class="slideshow">
+		<ul class="slider">
+			<li>
+		
+				<video  controls loop style="
+    width: 90%;
+    position: relative;
+    left: 3%;
+    top: 3vh;
+    
+"> <source src="video/video.mp4" type="video/mp4"></video>
+<section class="titulo">
+<h3>Inicio</h3>
+</section>
+			</li>
+			<li>
+				<video  controls loop style="
+    width: 90%;
+    position: relative;
+    left: 3%;
+    top: 3vh;
+    
+"> <source src="video/Proyecto.mp4" type="video/mp4"></video>
+<section class="titulo">
+<h3>Primer capitulo</h3>
+</section>
+		
+			</li>
+			<li style="width: 100%;">
+				<video  controls loop style="
+	width: 90%;
+    position: relative;
+    left: 3%;
+    top: 3vh;
+    
+"> <source src="video/instructivo.mp4" type="video/mp4"></video>
+<section class="titulo">
+<h3 class="ter">Segundo Capitulo</h3>
+</section>
+			
+			</li>
+		</ul>
+
+		<ol class="pagination">
+		
+		</ol>
+	
+	
+
+	</div>
 
 </section>
 
@@ -177,7 +265,7 @@ $row =mysqli_fetch_array($resultado);
         
 echo "<section class='row gale2'  >";
 
-     echo " <div class='col-12 col-sm-6 col-md-6 col-lg-5'>";
+     echo " <div class='col-12 col-sm-6 col-md-6 col-lg-6'>";
 
        echo " <div id='carouselExampleIndicators' class='carousel slide' data-ride='carousel'>";
          echo " <ol class='carousel-indicators'>";
@@ -222,8 +310,10 @@ echo "<section class='row gale2'  >";
         echo "<h3>" . $row['titulo'] . "</h3>";
 
         ?>
-
+         <br>
         <p><?php echo $row["detalles"];?></p>
+
+        <br>
 
 
         <button type="button" class="bt1 btn btn-info" data-toggle="modal" data-target="#exampleModalCenter">
@@ -262,39 +352,40 @@ echo "<section class='row gale2'  >";
       <div class="col-md-4 col-lg-3 mr-auto my-md-4 my-0 mt-4 mb-1">
           <h5 class="font-weight-bold text-uppercase mb-4">Información:</h5>
           <hr class="foot">
-          <p>Here you can use rows and columns here to organize your footer content.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit amet numquam iure provident voluptate esse
-            quasi, veritatis totam voluptas nostrum.</p>
+          <p class="nombres">En esta ágina encontraras la historia y el cuento de un búho llamado Andy la cual sufre de una discapacidad
+           auditiva y por ello se siente muy mal ya que le hace demasiado bullying.</p>
+          
 
         </div>
 
         <hr class="clearfix w-100 d-md-none">
-        <div class="col-md-2 col-lg-2 mx-auto my-md-4 my-0 mt-4 mb-1">
+        <div class=" col-md-2 col-lg-2 mx-auto my-md-4 my-0 mt-4 mb-1">
           <h5 class="font-weight-bold text-uppercase mb-4">Navegación:</h5>
           <hr class="foot">
 
-          <ul class="list-unstyled">
-            <li>
-              <p>
-                <a href="#!">Facebook</a>
-              </p>
-            </li>
-            <li>
-              <p>
-                <a href="#!">Instagram</a>
-              </p>
-            </li>
-            <li>
-              <p>
-                <a href="#!">Twitter</a>
-              </p>
-            </li>
-            <li>
-              <p>
-                <a href="#!">Pinterest</a>
-              </p>
-            </li>
-          </ul>
+          <ul class="list-unstyled list-inline text-center">
+    <li class="list-inline-item">
+      <a href="https://www.facebook.com/" class="btn-floating btn-fb mx-1">
+        <i class="fab fa-facebook-f"> </i>
+      </a>
+    </li>
+    <li class="list-inline-item">
+      <a href="https://twitter.com/" class="btn-floating btn-tw mx-1">
+        <i class="fab fa-twitter"> </i>
+      </a>
+    </li>
+    <li class="list-inline-item">
+      <a href="https://www.instagram.com/?hl=es-la" class="btn-floating btn-gplus mx-1">
+        <i class="fab fa-instagram"> </i>
+      </a>
+    </li>
+    <li class="list-inline-item">
+      <a href="https://co.pinterest.com/" class="btn-floating btn-li mx-1">
+        <i  class="fab fa-pinterest-square"> </i>
+      </a>
+    </li>
+    
+  </ul>
 
         </div>
 
@@ -304,14 +395,14 @@ echo "<section class='row gale2'  >";
           <h5 class="font-weight-bold text-uppercase mb-4">Creado Por:</h5>
           <hr class="foot">
 
-          <ul class="list-unstyled">
+          <ul class="nombres list-unstyled">
             <li>
               <p>
-                <i class="fa fa-home mr-3"></i> Faber Suárez</p>
+              <i class="fas fa-address-card"></i> - Faber Suárez</p>
                 <p>
-                  <i class="fa fa-home mr-3"></i> Daniel Perez</p>
+                <i class="fas fa-address-card"></i> - Daniel Perez</p>
                      <p>
-                        <i class="fa fa-home mr-3"></i> Kevin Rico</p>
+                     <i class="fas fa-address-card"></i> - Kevin Rico</p>
 
             </li>
           </ul>
@@ -325,10 +416,16 @@ echo "<section class='row gale2'  >";
 
 
     </div>
+
+
+
+
+  
+  
     
 
     <div class="footer-copyright text-center py-3">© 2019 Copyright:
-      <a href=""> Elsilenciomecrea@gmail.com</a>
+      <a href="https://mail.google.com/mail/u/0/?tab=wm#inbox"> Elsilenciomecrea@gmail.com</a>
       
     </div>
 
--- Proyecto/js/codigo.js
+++ Proyecto/js/codigo.js
@@ -51,3 +51,55 @@ $("#logo").animate({
 	"opacity": "1"
 }, 2000)
 
+
+$(".page-link").click(function(){
+
+     var videoAcargar = $(this).attr("video");
+
+     $("video").click(function(){
+          $("source src").text(videoAcargar)
+     
+     })
+})
+
+
+$(document).ready(function(){
+	var imgItems = $('.slider li').length; // Numero de Slides
+	var imgPos = 1;
+
+	// Agregando paginacion --
+	for(i = 1; i <= imgItems; i++){
+		$('.pagination').append('<li><span class="fa fa-circle"></span></li>');
+	} 
+	//------------------------
+
+	$('.slider li').hide(); // Ocultanos todos los slides
+	$('.slider li:first').show(); // Mostramos el primer slide
+	$('.pagination li:first').css({'color': 'rgb(24, 50, 99)'}); // Damos estilos al primer item de la paginacion
+
+	// Ejecutamos todas las funciones
+	$('.pagination li').click(pagination);
+
+
+	
+
+	// FUNCIONES =========================================================
+
+	function pagination(){
+		var paginationPos = $(this).index() + 1; // Posicion de la paginacion seleccionada
+
+		$('.slider li').hide(); // Ocultamos todos los slides
+		$('.slider li:nth-child('+ paginationPos +')').fadeIn(); // Mostramos el Slide seleccionado
+
+		// Dandole estilos a la paginacion seleccionada
+		$('.pagination li').css({'color': '#858585'});
+		$(this).css({'color': 'rgb(24, 50, 99)'});
+
+		imgPos = paginationPos;
+
+	}
+
+	
+
+});
+
